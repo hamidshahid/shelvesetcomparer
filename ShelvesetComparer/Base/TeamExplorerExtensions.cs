595bc9390e440d2a60fb8215b00c847af78c4ac3
dprZoft <73850562+dprZoft@users.noreply.github.com>
2021-05-02 12:29:14 +0200

    Update TeamExplorerExtensions.cs
    fix header for TeamExplorer extensions (used code from other repos)

--- ShelvesetComparer/Base/TeamExplorerExtensions.cs
+++ ShelvesetComparer/Base/TeamExplorerExtensions.cs
@@ -1,5 +1,8 @@
 ﻿// <copyright file="TeamExplorerExtensions.cs" company="https://github.com/dprZoft/shelvesetcomparer">
-// Copyright https://github.com/dprZoft/shelvesetcomparer, 2021. All Rights Reserved. 
+// Copyright https://github.com/hamidshahid/shelvesetcomparer.
+// Copyright https://github.com/rajeevboobna/shelvesetcomparer.
+// Copyright https://github.com/dprZoft/shelvesetcomparer, 2021.
+// All Rights Reserved. 
 // This code released under the terms of the Microsoft Public License (MS-PL, http://opensource.org/licenses/ms-pl.html). 
 // This is sample code only, do not use in production environments.
 // </copyright>
